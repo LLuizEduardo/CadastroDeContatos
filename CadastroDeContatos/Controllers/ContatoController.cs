2b4a05d40c3e682fda5af6a1a6b6b1c8841913e6
Luiz Eduardo Fernandes Junior <luiz.eduardo@aec.com.br>
2023-03-17 13:50:15 -0300

    Acoes e telas criadas

--- CadastroDeContatos/Controllers/ContatoController.cs
+++ CadastroDeContatos/Controllers/ContatoController.cs
@@ -12,5 +12,18 @@ namespace CadastroDeContatos.Controllers
         {
             return View();
         }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
     }
 }
