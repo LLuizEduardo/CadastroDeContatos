2828b7b22172dd737bbb41e99c0b0abd67498743
Luiz Eduardo Fernandes Junior <luiz.eduardo@aec.com.br>
2023-03-17 19:12:55 -0300

    Adicao servico de banco

--- CadastroDeContatos/Startup.cs
+++ CadastroDeContatos/Startup.cs
@@ -1,3 +1,4 @@
+using CadastroDeContatos.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +24,8 @@ namespace CadastroDeContatos
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            //adicionar o banco
+            services.AddEntityFrameworkSqlServer().AddDbContext<BancoContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
