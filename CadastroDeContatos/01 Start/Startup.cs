ff046d9a8e94ca621ff10ffbcceafdfc6175e811
Luiz Eduardo Fernandes Junior <luiz.eduardo@aec.com.br>
2023-03-19 11:45:06 -0300

    Organizacao

--- CadastroDeContatos/01 Start/Startup.cs
+++ CadastroDeContatos/01 Start/Startup.cs
@@ -1,6 +1,7 @@
 using CadastroDeContatos.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +26,7 @@ namespace CadastroDeContatos
         {
             services.AddControllersWithViews();
             //adicionar o banco
-            services.AddEntityFrameworkSqlServer().AddDbContext<BancoContext>();
+            services.AddEntityFrameworkSqlServer().AddDbContext<BancoContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DataBase")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
